1f4c7636d601f5647ad8c55d326eb649f1f9cf0d
Abdilahi Jama <abdijamzilan@gmail.com>
2022-05-28 12:38:02 -0500

    Added output

--- src/quadratic/Main.java
+++ src/quadratic/Main.java
@@ -1,5 +1,34 @@
 package quadratic;
 
 public class Main {
-    
+    /**
+     * Value of a.
+     */
+    private static final double A = 4.5;
+
+    /**
+     * Value of b.
+     */
+    private static final double B = 42;
+
+    /**
+     * Value of c.
+     */
+    private static final double C = 7.2;
+
+    /**
+     * Printing out the results.
+     * @param args arguments.
+     */
+    public static void main(String[] args){
+
+        // Creating an object.
+        TheQuadraticFunction quadraticFunction = new TheQuadraticFunction(A,B,C);
+
+        System.out.println(quadraticFunction.eval(1));
+        System.out.println(quadraticFunction.extremePoint());
+        System.out.println(quadraticFunction.shiftedAlongXBy(1));
+        System.out.println(quadraticFunction.shiftedAlongYBy(1));
+
+    }
 }
